f5b1c45da85b1352ad6ea20e881f97d53c57b2ba
wangwanneng <1079699925@qq.com>
2018-05-09 19:43:16 +0800

    change transform code

--- generateDataBatch.py
+++ generateDataBatch.py
@@ -1,10 +1,69 @@
 from config import *
 import os
 import random
-from skimage import io
+from skimage import io,transform
 import numpy as np
 from torchvision import transforms
-from PIL import Image
+
+#basic transform class
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        return img
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        return image
+
+
 
 class somethingBatch():
     def __init__(self, label, train, test, validation, datapath):
@@ -63,9 +122,16 @@ class somethingBatch():
         minivideo = io.imread(video_path + selected_files[0])#shape: height * width * channel
         for i in range(1,len(selected_files)):
             minivideo = np.concatenate((minivideo, io.imread(video_path + selected_files[i])),axis=2)
-        minivideo = Image.fromarray(minivideo)
+
+        rescale = Rescale(256)
+        crop = RandomCrop(224)
+
+        minivideo = rescale(minivideo)
+        minivideo = crop(minivideo)
+        if random.random() > 0.5:
+            minivideo = minivideo[:,::-1,:]
         #minivideo shape: height * width * channel (also PIL image format)
-        tsfm = transforms.Compose([transforms.Scale(256), transforms.RandomCrop(224), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406]*15, std=[0.229, 0.224, 0.225]*15)])
+        tsfm = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406]*15, std=[0.229, 0.224, 0.225]*15)])
         #torch tensor default require grad is false
         minivideo_transformed = tsfm(minivideo)#normalization mean and std which is from imagenet
         #add extra axis
